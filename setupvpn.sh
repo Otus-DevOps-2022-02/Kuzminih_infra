46c514159833b2c5e903a2836f73d16a4bc9e12d
Fedor <kfa84@mail.ru>
2022-04-10 12:53:00 +0800

    hw3 add cloud_bastion.ovpn setupvpn.sh

--- setupvpn.sh
+++ setupvpn.sh
@@ -5,8 +5,6 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 gpg --keyserver hkp://keyserver.ubuntu.com --recv 7AE645C0CF8E292A
 gpg --export --armor 7AE645C0CF8E292A | sudo apt-key add -
-sudo apt update
-sudo apt --assume-yes install pritunl mongodb-server
 apt-get --assume-yes update
 apt-get --assume-yes upgrade
 apt-get --assume-yes install pritunl mongodb-org
